3e86ce416546af929895564550197ef86255e639
richard.suprayogi <richardlim97@rocketmail.com>
2022-04-23 11:11:36 +0700

    async await feature and err validation

--- sandbox.js
+++ sandbox.js
@@ -39,6 +39,24 @@
 //     })    
 // }
 
+
+//async function
+const getTodosPromiseAsync = async () => {
+    //no need to specify then catch, it has covered by await keyword to resolve promise result  
+    const resp = await fetch('sandbox.json');
+
+    //based on "condition when a promise get rejected", a promise still possible to be rejected
+    //so need to add validation here before causing err resolve values into json
+    if(resp.status !== 200){
+        throw new Error('cannot fetch data');
+    }
+
+    //resp still a promise, to return data, you need resolve values
+    const extracted = await resp.json();
+
+    return extracted;
+}
+
 console.log(1);
 // getTodos('https://jsonplaceholder.typicode.com/todos/1',(err, data) => {
 // if(err){
@@ -75,15 +93,27 @@ console.log(1);
 // });
 
 //fetch api
-//when it get reject, https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-fetch('sandbox.json').then(resp => {
-    console.log(resp);
-    // resolve promise object, convert into json
-    return resp.json();
-}).then(json => {
-    console.log(json);
-}).catch(err => {
-    console.log(err);
-});
+//condition when a promise get rejected : 
+// A fetch() promise only rejects when a network error is encountered (which is usually when there's a permissions issue or similar). A fetch() promise does not reject on HTTP errors (404, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
+// source https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+// fetch('sandbox.json').then(resp => {
+//     console.log(resp);
+//     // resolve promise object, convert into json
+//     return resp.json();
+// }).then(json => {
+//     console.log(json);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+//async await, async func always return promise
+//non blocking method
+//still need to resolve return values
+//async await does not supported by OLD BROWSER, like i.e
+getTodosPromiseAsync()
+    .then(resp => console.log(resp))
+    //catch part, print message err
+    .catch(err => console.log(err.message));
 
 console.log(2);
